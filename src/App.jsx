11c60cbe7fd6d9428a3aad52b6776012d42a781c
elton-souza <elton.souza1515@gmail.com>
2022-01-04 10:40:57 -0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./assets/css/base/base.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./paginas/Home";
+import Sobre from "./paginas/Sobre";
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/sobre">
+          <Sobre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
