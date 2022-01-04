8f2652df161570a91e3aa46cacdd72b720b5a1cf
elton-souza <elton.souza1515@gmail.com>
2022-01-04 11:40:32 -0300

    adicionando header

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import "./assets/css/base/base.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
+import Pagina404 from "./paginas/Pagina404";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/sobre">
-          <Sobre />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="*" element={<Pagina404 />} />
+      </Routes>
+    </>
   );
 }
 
